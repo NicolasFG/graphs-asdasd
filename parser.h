1f0a2fdddac259232e0e73b9a2209ed559fbda79
Jonathan Prieto <jonathan.prieto@me.com>
2019-10-23 14:39:04 -0500

    First Try Parser

--- parser.h
+++ parser.h
@@ -1,7 +1,20 @@
 #ifndef GRAPHS_ASDASD_PARSER_H
 #define GRAPHS_ASDASD_PARSER_H
-#include "json.hpp"
 
+#include <iostream>
+#include "json.hpp"
+#include <fstream>
+
+using namespace std;
+
+// read a JSON file
+ifstream i("airports.json");
+nlohmann::json j;
+i >> j;
+
+// write prettified JSON to another file
+ofstream o("pretty.json");
+o << std::setw(4) << j << std::endl;
 
 
 
