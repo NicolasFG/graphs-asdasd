92e67aa77eb261e13329b1aacdc948c671001b5f
Jonathan Prieto <jonathan.prieto@me.com>
2019-10-23 13:29:34 -0500

    Upload Files

---
+++ cmake-build-debug/CMakeFiles/graphs-asdasd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graphs-asdasd.dir/main.cpp.o"
+  "graphs-asdasd"
+  "graphs-asdasd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graphs-asdasd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
