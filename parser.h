fe82e51d1a004c27f8b731930fe3d392d9ae03d8
Jonathan Prieto <jonathan.prieto@me.com>
2019-10-24 18:39:49 -0500

    Push Main

--- parser.h
+++ parser.h
@@ -7,15 +7,11 @@
 
 using namespace std;
 
-// read a JSON file
-ifstream i("airports.json");
-nlohmann::json j;
-i >> j;
-
-// write prettified JSON to another file
-ofstream o("pretty.json");
-o << std::setw(4) << j << std::endl;
-
-
+int main(){
+    string filename="airports.json";
+    ifstream file_input(filename);
+    nlohmann::json airports;
+    file_input >> airports;
+}
 
 #endif //GRAPHS_ASDASD_PARSER_H
