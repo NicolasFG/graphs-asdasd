56a46f0014608aefe621297eaac33887e537292c
Francesco Uccelli <39736679+Flrotm@users.noreply.github.com>
2019-10-29 14:47:20 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 void trials(){
-    graph G(true);
+    graph G(false);
 
     G.createNodes(0, "LA", 120.0, 20);
     G.createNodes(1, "Lima", 50.0, 30);
@@ -17,21 +17,41 @@ void trials(){
     G.createConection(2,3, 1);
     G.createConection(3,0, 1);
     G.printLA();
-    G.printNode(0);
+   // G.printNode(0);
     G.removeNode(0);
-    G.printNode(0);
-    G.printArista(0,1);
-    G.printArista(3,0);
+    //G.printNode(0);
+    //G.printArista(0,1);
+    //G.printArista(3,0);
 
+    vector<bool> discovered;
+    vector<int> color(G.getNodes());
+    color[0]=0;
+
+   discovered= G.fillvectordiscovered(G,discovered);
+
+    bool a= graph::is_bipartite(G,0,discovered,color);
+
+    if(a){
+        cout<<" es bipartido";
+    }
+
+    else{
+        cout<< " no es bipartido ";
+    }
+cout<<endl;
+    G.getKruskal();
 
 
 
 }
 
+
+
 int main(){
     //fillin();
-    createjsonnodes();
-    //trials();
+   // createjsonnodes();
+    trials();
+
 
     return 0;
 }
