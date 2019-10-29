bb38e1ecfac85e8a6e2076cb83d395e1fa029ae4
Francesco Uccelli <39736679+Flrotm@users.noreply.github.com>
2019-10-29 14:46:40 -0500

    Update graph.h

--- graph.h
+++ graph.h
@@ -28,7 +28,8 @@ class graph {
 private:
     unsigned int nodes,edges{};
     bool is_directed;
-    vector<vector<Node*>> LA;
+    Edge* edge;
+     vector<vector<Node*>> LA;
 
 public:
     explicit graph(bool isdirected);
@@ -38,29 +39,39 @@ public:
     void createConection(int origin, int end);
     void createConection(int origin, int end, double pond);
     void removeNode(int _id);
+
     void removeConnection(int OriginKey, int EndKey);
+    unsigned int getNodes();
+
 
     Node* findNode(int key);
     Edge* findArista(int OriginKey, int EndKey);
 
-    double calculateDensity();  
+    double calculateDensity();
     static string denseOrDispersed(double densidad, double cota);
     bool isConexo();
     bool isFuertementeConexo();
-    bool is_bipartite(graph G,int n);
+    static bool is_bipartite(graph G,int n,vector<bool>& discovered,vector<int>& color);
+    static vector<bool> fillvectordiscovered(graph G,vector<bool> vector);
+    vector<Edge*> filledges();
 
-    string getPrim();
+
     string getKruskal();
+
 
     int getNodeIdByName(const string& Name);
     double calculatedistance(int key1, int key2);
     void printNode(int key);
     void printArista(int OriginKey, int EndKey);
     void printLA();
-    void filledges();
+
+
     int minKey(vector<int> key, vector<bool> mstSet);
     void printMST(const vector<int>& parent);
     void primMST();
+
+
+
 
 
 };
